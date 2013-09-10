abe480079c47d8c81c2a93ca06613145852c0fa8
Arthur S. Lamorz <arthur.lamorz@gmail.com>
2013-09-10 20:58:03 +0800

    Init BCRound

---
+++ src/com/lamorz/bcsim/bcstruct/BCRound.java
@@ -0,0 +1,68 @@
+package com.lamorz.bcsim.bcstruct;
+
+import com.lamorz.bcsim.BCBetTree;
+
+public class BCRound {
+
+	private final int GAMES_PER_ROUND = 4;
+	private BCGame[] m_games;
+	
+	private int m_currentGame;
+	private int m_betLevel;
+	private int m_gainFromLastRound;
+	private BCBetTree m_betTree;
+	
+	public BCRound()
+	{
+		init(0);
+	}
+	
+	public BCRound(int gainFromLastRound)
+	{
+		init(gainFromLastRound);
+	}
+	
+	private void init(int gainFromLastRound)
+	{
+		m_games = new BCGame[GAMES_PER_ROUND];
+		m_currentGame = 0;
+		m_betLevel = 1;
+		m_gainFromLastRound = gainFromLastRound;
+	}
+	
+	public int playAllGames()
+	{
+		for (m_currentGame=0; m_currentGame < GAMES_PER_ROUND; m_currentGame++)
+		{
+			BCGame game = new BCGame(m_betLevel);
+			m_games[m_currentGame] = game;
+			
+			game.playAllHands();
+		}
+		
+		return m_currentGame;
+	}
+
+	public BCGame playSingleGame()
+	{
+		if (m_currentGame == GAMES_PER_ROUND)
+			return null;
+		
+		BCGame game = new BCGame(1);
+		game.playAllHands();
+		
+		m_games[m_currentGame] = game;
+		m_currentGame++;
+		
+		return game;
+	}
+	
+	public int getBetLevel() {
+		return m_betLevel;
+	}
+
+	public void setBetLevel(int betLevel) {
+		m_betLevel = betLevel;
+	}
+
+}
