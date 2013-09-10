95ffeaa58d00f385be4aa156d5266cc493bd1c54
Arthur S. Lamorz <arthurlamorz@gmail.com>
2013-09-10 12:48:17 +0800

    bcstruct initail

---
+++ src/com/lamorz/bcsim/bcstruct/BCGame.java
@@ -0,0 +1,43 @@
+package com.lamorz.bcsim.bcstruct;
+
+import com.lamorz.bcsim.BCBetTree;
+
+public class BCGame {
+	
+	private final int HANDS_PER_GAME = 5;
+	private BCHand[] m_hands;
+	private int m_currentHand;
+	private int m_betLevel;
+	private BCBetTree m_betTree;
+	
+	public BCGame()
+	{
+		m_hands = new BCHand[HANDS_PER_GAME];	
+		m_currentHand = 0;
+		m_betLevel = 1;
+		m_betTree = new BCBetTree(); 
+	}
+	
+	public BCHand playSingleHand()
+	{
+		if (m_currentHand == HANDS_PER_GAME)
+			return null;
+		
+		BCHand hand = new BCHand();
+		hand.play(m_currentHand, m_betLevel);
+		
+		m_hands[m_currentHand] = hand;
+		m_currentHand++;
+		
+		return hand; 
+	}
+	
+	public int playAllHands()
+	{
+		while (m_currentHand < HANDS_PER_GAME)
+			playSingleHand();
+		
+		return m_currentHand;
+	}
+
+}
--- src/com/lamorz/bcsim/bcstruct/BCHand.java
+++ src/com/lamorz/bcsim/bcstruct/BCHand.java
@@ -1,4 +1,4 @@
-package com.lamorz.bcsim;
+package com.lamorz.bcsim.bcstruct;
 
 import java.util.Random;
 
