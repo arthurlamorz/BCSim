ded6ad3b7efcd7d3f4f68a62ec36f33ed03ba4a7
Arthur S. Lamorz <arthur.lamorz@gmail.com>
2013-09-11 09:03:03 +0800

    BC Tree Manager

---
+++ src/com/lamorz/bcsim/BCBetTreeManager.java
@@ -0,0 +1,13 @@
+package com.lamorz.bcsim;
+
+public class BCBetTreeManager {
+	
+	public static final int TOTAL_BET_LEVELS = 8;
+	private BCBetTree[] m_betTrees;
+	
+	public BCBetTreeManager()
+	{
+		m_betTrees = new BCBetTree[TOTAL_BET_LEVELS];
+	}
+	
+}
